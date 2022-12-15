2f05dc0ab7571e899bcd9cff5aece40081ac0ed1
Hannah Hoang <112083555+hannahhoang2704@users.noreply.github.com>
2022-12-15 17:40:29 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,118 +1,55 @@
-'use strict';
+const searchFormElem = document.querySelector("form");
+const showsContainerElem = document.querySelector("#shows");
+const dialogElem = document.querySelector("dialog");
+const closeModelElem = document.querySelector("#closeModel");
+const modalTitleElem = document.querySelector("#modalTitle");
 
-let keyword = document.getElementById('query');
-const button = document.querySelector('input[type=submit]');
-const section = document.getElementById('result');
+closeModelElem.addEventListener("click", () => dialogElem.close());
 
-function fetch_data(jsonData){
-  for (let i = 0; i <jsonData.length; i++){
+searchFormElem.addEventListener("submit", async function(submitEvent) {
+  submitEvent.preventDefault();
+  const formData = new FormData(this);
+  const search = new URLSearchParams(formData).toString();
+  const fetchData = await fetch(this.action + search);
+  const jsonData = await fetchData.json();
+  
+  if(jsonData.length) showsContainerElem.innerHTML = "";
+  else showsContainerElem.innerHTML = "<p>No results</p>";
+  jsonData.forEach(result => {
+    const {show} = result;
+    const showContainerElem = document.createElement("div");
+    showContainerElem.classList.add("show");
+    const nameElem = document.createElement("h2");
+    nameElem.textContent = show.name;
+  
+    const moreDetailsElem = document.createElement("a");
+    moreDetailsElem.href = show.url;
+    moreDetailsElem.target = "details";
+    moreDetailsElem.textContent = "More details";
 
-              //console.log(jsonData[i].name);
-              const smallDiv = document.createElement('article');         //create an article for every movie
-              section.appendChild(smallDiv);
-              let idOfArticle = `article-${i+1}`;
-              smallDiv.setAttribute('id', `article-${i+1}`);
-
-              const p = document.createElement('h3');                     //make movies' title
-              const name = document.createTextNode(`${jsonData[i].name}`);
-              p.appendChild(name);
-              smallDiv.appendChild(p);
-
-              const a = document.createElement('a');                      //create a link
-              a.href = jsonData[i].url;
-              a.setAttribute('target', '_blank');
-              smallDiv.appendChild(a);
-              a.appendChild(p);
-
-              /*const dialog = document.createElement('dialog');            //create dialog for modal
-              smallDiv.appendChild(dialog);
-              const span = document.createElement('span');
-              span.appendChild(document.createTextNode('&#x2715;'));
-              dialog.appendChild(span);
-              const iframe = document.createElement('iframe');            //create iframe in dialog
-              dialog.appendChild(iframe);
-              iframe.src = 'https://www.google.com';*/
-
-
-              const img = document.createElement('img');                  //create image for the movie
-              img.setAttribute('id', `img-${i+1}`)
-              img.src = `${jsonData[i].img}`;
-              if (jsonData[i].img ===""){                                         //if movie doesn't have image, put default image
-                img.src = 'https://via.placeholder.com/100x200?text=text+here';
-              }
-              img.alt = jsonData[i].name;
-              smallDiv.appendChild(img)
-
-
-              const genres = document.createElement('p');             //add genres for movie
-              genres.setAttribute('class', 'genres')
-              let genresList = jsonData[i]['genres'];                         //Fetch genres of the movies
-              let text = genresList.toString();
-              const newText = text.replace(/,/g, ' | ');    //change ',' to '|'
-              //console.log(newText);
-              genres.appendChild(document.createTextNode(newText))
-              smallDiv.appendChild(genres);
-
-              if (jsonData[i].summary == null){                                 //add summary for the movie
-                smallDiv.innerHTML += `<br>${jsonData[i].name}`;
-              }else {
-                smallDiv.innerHTML += `${jsonData[i].summary}`;
-              }
-
-            //add iframe and modal
-    }
-
-}
-
-
-function modal(evt){
-  evt.preventDefault();
-  for (let i = 0; i <jsonData.length; i++){
-    let idOfImg = `img-${i+1}`;
-    const callImg = document.getElementById(idOfImg);
-    const dialog = document.querySelector('dialog');
-    const span = document.querySelector('span');
-    const iframe = document.querySelector('iframe');
-    callImg.addEventListener('click', ()=>{
-      iframe.src = `${jsonData[i].url}`;
-      dialog.showModal();
-
-    })
-
-    span.addEventListener('click', ()=>{
-      dialog.close();
-    })
-  }
-}
-const img = document.createElement('img');
-img.addEventListener('click', modal);
-
-
-function search_result(evt){
-  evt.preventDefault();
-
-  //console.log(keyword.value);
-  asynchronousFunction();
-
-}
-
-
-console.log('the script starts');
- async function asynchronousFunction() {                 // asynchronous function is defined by the async keyword
-        section.innerHTML = '';
-        //console.log('asynchronous download begins');
-        try {                                               // error handling: try/catch/finally
-            const response = await fetch('http://127.0.0.1:8000/' + keyword.value);    // starting data download, fetch returns a promise which contains an object of type 'response'
-            const jsonData = await response.json();          // retrieving the data retrieved from the response object using the json() function
-            //console.log(jsonData[0].name);
-            fetch_data(jsonData);
-        } catch (error) {
-            console.log(error.message);
-        } finally {                                         // finally = this is executed anyway, whether the execution was successful or not
-            console.log('asynchronous load complete');
-        }
-    }
-
-//console.log('the script ends');
-
-button.addEventListener('click', search_result);
+    moreDetailsElem.addEventListener("click", () => {
+      dialogElem.showModal();
+      modalTitleElem.textContent = show.name;
+    });
+  
+    const img = document.createElement("img");
+    img.src = show.image?.medium ?? "https://via.placeholder.com/100x200?text=text+here";
+    
+    const genreContainer = document.createElement("div");
+    genreContainer.innerHTML += "<h2>Genres</h2>"
+    genreContainer.classList.add("genres");
+    show.genres.forEach(genre => {
+      const genreElem = document.createElement("p");
+      genreElem.textContent = genre;
+      genreElem.classList.add("genre");
+      genreContainer.appendChild(genreElem);
+    });
+  
+    const summaryElem = document.createElement("div");
+    summaryElem.classList.add("summary");
+    summaryElem.innerHTML = show.summary;
+    showContainerElem.append(nameElem, img, summaryElem, moreDetailsElem);
+    if(show.genres?.length) img.after(genreContainer)
+    showsContainerElem.append(showContainerElem);
+  });
+});
